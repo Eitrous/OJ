9936797faf0df6989700d8b5cdc215057f84700b
Eitrous <hzx.zxhuang@outlook.com>
2025-11-12 21:19:27 +0800

    modified:   yoj/1221.cpp 	new file:   yoj/2025midB/1271.cpp 	new file:   yoj/2025midB/1307.cpp 	new file:   yoj/2025midB/1569.cpp 	new file:   yoj/2025midB/1570.cpp 	new file:   yoj/2025midB/1574.cpp 	new file:   yoj/2025midB/1579.cpp 	new file:   yoj/270.cpp

--- yoj/1221.cpp
+++ yoj/1221.cpp
@@ -1,105 +1,94 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-struct Poker
-{
-    char type = 0;
+char value[5] = "JQKA";
+char type[5] = "HDCS";
+
+struct Poker{
+    char type = ' ';
     int value = 0;
-    bool must_use = false;
-};
+    int typeno = -1;
+    bool mstus = false;
+}poker[8];
 
 
-int main()
-{
-    int n = 0;
-    scanf("%d",&n);
-    while (n--)
-    {
+int main(){
+    int n;cin >> n;
+    int pos = 0,no;
+    string str;
+    while (n--){
+        //输入
         getchar();
-        Poker poker[10];
-        int count[256] = {0},cnt = 0,mark[5] = {0}; //H72 D68 C67 S83
-        bool is_TH = false,is_SZ = true,must_used = false;
-        string str;
-        int pos = 0;
+        pos = 0,no = 1;
         getline(cin,str);
-        str += "    ";
-        for (int i = 1; i <= 7; i++)
-        {
-            if(i <= 2) poker[i].must_use = true;
-            poker[i].type = str[pos];
+        str += "   ";
+        while (pos < str.length()){
+            if(no <= 2) poker[no].mstus = true;
+            poker[no].type = str[pos];
+            for(int i = 0; i < 4; i++){
+                if(poker[no].type == type[i]) poker[no].typeno = i;
+                break;
+            }
             pos++;
-            if(str[pos] > '9') poker[i].value = (str[pos] == 'J' ? 11 : (str[pos] == 'Q' ? 12 : (str[pos] == 'K' ? 13 : 14)));
-            else
-            {
-                poker[i].value = str[pos]-'0';
-                if(str[pos+1] == '0') 
-                {
-                    poker[i].value*10;
+            if(str[pos] > '9'){
+                for (int i = 0; i < 4; i++){
+                    if(str[pos] == value[i]) {
+                        poker[no].value = 11+i;
+                        pos++;
+                        break;
+                    }
+                }
+            }
+            else{
+                poker[no].value = str[pos]-'0';
+                pos++;
+                if(str[pos] != ' '){
+                    poker[no].value *= 10;
                     pos++;
                 }
             }
-            pos += 2;
-            count[poker[1].type]++;
+            pos++;
+            no++;
         }
-        
-        if(poker[1].type == poker[2].type && count[poker[1].type] >= 5) is_TH = true;
 
-        for (int i = 1; i <= 6; i++)
-        {
-            for (int j = 1; j <= 7 - i; j++)
-            {
-                if(poker[j+1].value < poker[j].value)
-                {
-                    poker[0] = poker[j];
-                    poker[j] = poker[j+1];
-                    poker[j+1] = poker[0];
-                }
+        //处理
+        int valuecnt[15][8] = {{0}},typcnt[5][8] = {{0}};
+        char th_typ = poker[1].type;
+        bool is_th = true,is_sz = false,is_ths = false;
+
+        if(poker[2].type != th_typ) is_th = false;
+
+        for (int i = 1; i <= 7; i++){
+            valuecnt[poker[i].value][0]++;
+            valuecnt[poker[i].value][valuecnt[poker[i].value][0]] = i;
+            typcnt[poker[i].typeno][0]++;
+            typcnt[poker[i].typeno][typcnt[poker[i].typeno][0]] = i;
+        }
+
+        for (int i = 1; i <= 3; i++){
+            bool judge = true;
+            for (int j = i; j <= i+3; j++){
+                if(valuecnt[j][0] != valuecnt[j+1][0] - 1) judge = false;
             }
-            
-        }
-
-        for (int i = 1; i <= 3; i++)
-        {
-            is_SZ = true;
-            must_used = false;
-            for (int j = i; j <= i+3; j++)
-            {
-                if(poker[j].must_use) must_used = true;
-                if(poker[j].value != poker[j+1].value-1)
+            if(judge){//当可能有sz时
+                //判断有无必用牌
+                bool is_mst = false;
+                for (int j = i; j <= i+4; j++)
                 {
-                    is_SZ = false;
+                    if()
+                }
+                
+                for (int j = i; j <= i+4; j++)
+                {
                     
                 }
                 
             }
-            if(is_SZ && must_used) mark[++cnt] = i;
         }
-        if(cnt > 0) is_SZ = true;
-        else is_SZ = false;
-
-        if(!is_SZ && !is_TH) printf("GP\n");
-        else if(!is_SZ && is_TH) printf("TH\n");
-        else if(is_SZ && !is_TH) printf("SZ\n");
-        else
-        {
-            for (int i = 1; i <= cnt; i++)
-            {
-                is_TH = true;
-                for (int j = mark[i]; j <= mark[i] + 3; j++)
-                {
-                    if(poker[j].type != poker[j+1].type) is_TH = false;
-                }
-                if(is_TH)
-                {
-                    printf("THS\n");
-                    break;
-                }
-                
-            }
-            if(!is_TH) printf("TH\n");
-        }
+        
+        
         
 
     }
-    
+    return 0;
 }
